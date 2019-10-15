8cb6c30e5055d923963fd9a4db5304a7d11cad6f
danielfurnivall <danielfurnivall@gmail.com>
2019-10-15 11:38:38 +0100

    Completed Section 1 entirely

--- src/Week4Q1.java
+++ src/Week4Q1.java
@@ -29,7 +29,16 @@ public class Week4Q1 {
         }
         System.out.println("Array 2 Length = " + intArray.length);
         System.out.println("Array 2 Average = "+ intaverage(intArray));
-        System.out.println("Value 16 found at position " + position(intArray, 16));
+        System.out.println("Value 16 found at position " + firstposition(intArray, 16));
+        System.out.println("Value 16 last position : " + lastposition(intArray, 16));
+        System.out.println("Value 16 found : " + intcount(intArray, 16) + " times");
+        int[] occurencesArray = arrayOfOccurences(intArray, 16);
+        for(int i = 0; i < occurencesArray.length; i++){
+            if (occurencesArray[i] != -1){
+                System.out.println(occurencesArray[i]);
+
+            }
+        }
     }
     private static double average(double[] array){
         double counter = 0;
@@ -46,7 +55,7 @@ public class Week4Q1 {
         return (counter / array.length);
     }
 
-    private static int position(int[] array, int number){
+    private static int firstposition(int[] array, int number){
         for(int i = 0; i < array.length; i++){
             //iterate through array
             if (array[i] == number) {
@@ -55,6 +64,39 @@ public class Week4Q1 {
         }
         return -1;
         }
+    private static int lastposition(int[] array2, int number2) {
+        int current = 0;
+        for (int i = 0; i < array2.length; i++) {
+            if (array2[i] == number2) {
+                System.out.println(i);
+                current = i;
+            } else {
+                continue;
+            }
 
+        }
+        return current;
+    }
+    private static int intcount(int[] array3, int number3){
+        int count = 0;
+        for (int i = 0; i < array3.length; i++){
+            if (array3[i] == number3){
+                count +=1;
+            }
+        }
+        return count;
+    }
+    private static int[] arrayOfOccurences(int[] array4, int num){
+        int array[] = new int[10000];
+        for (int i=0; i<array.length; i++){
+            array[i] = -1;
+        }
+        for (int i = 0; i < array4.length; i++){
+            if (array4[i] == num){
+                array[i] = i;
+            }
+        }
+        return array;
+    }
 }
 
